eafac3c815627c218ae7cfa24f6bb2d50f4eebc1
LysieSoyez <Lysie.a.soyez@gmail.com>
2023-01-17 09:56:20 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <title>AZ[Store]</title>
+</head>
+<body>
+    <!-- nav -->
+    <nav>
+    </nav>
+
+    <!--Header (shoe the right one)-->
+    <header>
+    </header>
+
+    <!--main (our last products)-->
+    <main>
+
+    <!--Comments section-->
+
+    </main>
+    
+    <!-- footer-->
+    <footer>
+    </footer>
+</body>
+</html>
