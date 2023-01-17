8805b52bbc44473c81a0a21cbc1ff4653f610cdc
Laura Wilhelmi <laurawilhelmi.pro@gmail.com>
2023-01-17 10:52:53 +0100

    tentatite de array

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./assets/style.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
     <title>AZ[Store]</title>
 </head>
 <body>
@@ -19,63 +19,7 @@
     <!--main (our last products)-->
     <main>
 
-    <div class="shop__bestquality">
-    <img src="./assets/images/shoe_two.png">
-    <h2>We provide you the <span>best</span> quality</h2>
-    <p class="">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Beatae dolores quae possimus tenetur veniam ullam quo, minus, 
-        corporis a, temporibus cumque dignissimos expedita praesentium 
-        quis sint voluptas quos. Rem officia sint nemo voluptatum 
-        voluptatem. Laboriosam veritatis sunt cupiditate adipisci 
-        natus nisi vel maxime fugit voluptas. Aliquid incidunt dicta
-        eos facere. Repellendus ut optio nam, ipsam cupiditate non maxime?
-    </p>
-<div class="">
-    <div class="">
-        <img src="./assets/images/image-emily.jpg">
-            <b>Emily from xyz</b>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing 
-                elit. Cupiditate error unde debitis optio, officiis 
-                provident quisquam minus vel omnis iure porro? 
-                Consequuntur illum aspernatur nesciunt veritatis 
-                animi recusandae molestiae inventore similique, 
-                commodi atque iure veniam dignissimos pariatur quasi
-                 voluptatum? Culpa, repudiandae temporibus ad 
-                 ullam dolor alias! Eum!
-            </p>
-    </div>
-    <div class="">
-        <img src="./assets/images/image-thomas.jpg">
-            <b>Thomas from corporate</b>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing 
-                elit. Cupiditate error unde debitis optio, officiis 
-                provident quisquam minus vel omnis iure porro? 
-                Consequuntur illum aspernatur nesciunt veritatis 
-                animi recusandae molestiae inventore similique, 
-                commodi atque iure veniam dignissimos pariatur quasi
-                 voluptatum? Culpa, repudiandae temporibus ad 
-                 ullam dolor alias! Eum!
-            </p>
-    </div>
-    <div class="">
-        <img src="./assets/images/image-jennie.jpg">
-            <b>Jennie from Nike</b>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing 
-                elit. Cupiditate error unde debitis optio, officiis 
-                provident quisquam minus vel omnis iure porro? 
-                Consequuntur illum aspernatur nesciunt veritatis 
-                animi recusandae molestiae inventore similique, 
-                commodi atque iure veniam dignissimos pariatur quasi
-                 voluptatum? Culpa, repudiandae temporibus ad 
-                 ullam dolor alias! Eum!
-            </p>
-    </div>
-
-</div>
-    </div>
-
-
+    <!--Comments section-->
 
     </main>
     
