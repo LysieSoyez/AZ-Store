0b44ca9d50bf5d0767ad237a651a11d45761cbd8
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-01-17 13:59:16 +0100

    styles

--- index.php
+++ index.php
@@ -46,55 +46,36 @@
     <div class="shop__testimonials">
     <div class="testimonials__header">
     <img src="./assets/images/shoe_two.png">
-    <h2>We provide you the <span>best</span> quality</h2>
+    <h2>We provide you the <span>best</span> quality.</h2>
     <p class="">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
         Beatae dolores quae possimus tenetur veniam ullam quo, minus, 
-        corporis a, temporibus cumque dignissimos expedita praesentium 
-        quis sint voluptas quos. Rem officia sint nemo voluptatum 
-        voluptatem. Laboriosam veritatis sunt cupiditate adipisci 
-        natus nisi vel maxime fugit voluptas. Aliquid incidunt dicta
-        eos facere. Repellendus ut optio nam, ipsam cupiditate non maxime?
+        corporis a, tdolores quae poss
     </p>
 </div>
 <div class="testimonials__people">
-    <div class="">
+    <div>
         <img src="./assets/images/image-emily.jpg">
             <b>Emily from xyz</b>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing 
                 elit. Cupiditate error unde debitis optio, officiis 
-                provident quisquam minus vel omnis iure porro? 
-                Consequuntur illum aspernatur nesciunt veritatis 
-                animi recusandae molestiae inventore similique, 
-                commodi atque iure veniam dignissimos pariatur quasi
-                 voluptatum? Culpa, repudiandae temporibus ad 
-                 ullam dolor alias! Eum!
+                providen
             </p>
     </div>
-    <div class="">
+    <div>
         <img src="./assets/images/image-thomas.jpg">
             <b>Thomas from corporate</b>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing 
                 elit. Cupiditate error unde debitis optio, officiis 
-                provident quisquam minus vel omnis iure porro? 
-                Consequuntur illum aspernatur nesciunt veritatis 
-                animi recusandae molestiae inventore similique, 
-                commodi atque iure veniam dignissimos pariatur quasi
-                 voluptatum? Culpa, repudiandae temporibus ad 
-                 ullam dolor alias! Eum!
+                providen
             </p>
     </div>
-    <div class="">
+    <div>
         <img src="./assets/images/image-jennie.jpg">
             <b>Jennie from Nike</b>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing 
                 elit. Cupiditate error unde debitis optio, officiis 
-                provident quisquam minus vel omnis iure porro? 
-                Consequuntur illum aspernatur nesciunt veritatis 
-                animi recusandae molestiae inventore similique, 
-                commodi atque iure veniam dignissimos pariatur quasi
-                 voluptatum? Culpa, repudiandae temporibus ad 
-                 ullam dolor alias! Eum!
+                providen
             </p>
     </div>
 
