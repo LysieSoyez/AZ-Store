83c41cf6d21d31492c6677d4a7af8b488203999b
Corentin Miserque <miserquecorentin@gmail.com>
2023-01-18 16:42:24 +0100

    add css shopping card

--- shopping-cart.php
+++ shopping-cart.php
@@ -143,7 +143,7 @@ echo '<div class="cart__line">';
                 echo '<div class="cart__line">';
                     echo '<span class="name_product">'.$items[$cart['pro_id']-1]['product'].'</span>';
                     echo '<span class="price_product">'.$items[$cart['pro_id']-1]['price'].' $</span><br>';
-                    echo '<span> quantity:  '.$cart['qty'].'</span>';
+                    echo '<span class="quantity_product"> quantity:  '.$cart['qty'].'</span>';
                     echo '<form method="post"><input type="submit" value="-"></form>';
                     echo '</div>';
                 }
